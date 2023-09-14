3140c5f761119d63a9c6ef7ba4a6ed4f1bf11d7a
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-09-14 09:54:48 -0400

    fixing urls.py

--- newenv/urls.py
+++ newenv/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import url
+from django.urls import path, include
 
 
 urlpatterns = [
     
     url(r"^admin/", admin.site.urls),
-    url(r"^", include("TweetWiz.urls")),
+    path("", include("TweetWiz.urls")),
 ]
